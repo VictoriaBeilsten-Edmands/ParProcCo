82c6b62c914f7eee657a0b9319c9de4933c48a57
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2021-08-26 16:29:11 +0100

    Add SimpleDataAggregator and tests

---
+++ ParProcCo/simple_data_aggregator.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import List
+
+
+class SimpleDataAggregator:
+
+    def __init__(self, total_slices: int) -> None:
+        if type(total_slices) is int:
+            self.total_slices = total_slices
+        else:
+            raise TypeError(f"total_slices is {type(total_slices)}, should be int\n")
+
+    def aggregate(self, aggregation_output_dir: Path, output_data_files: List[Path]) -> Path:
+        aggregated_data_file = Path(aggregation_output_dir) / "aggregated_results.txt"
+        if len(output_data_files) != self.total_slices:
+            raise ValueError(
+                f"Number of output files {len(output_data_files)} must equal total_slices {self.total_slices}")
+        aggregated_lines = []
+        for output_file in output_data_files:
+            with open(output_file) as f:
+                for line in f.readlines():
+                    aggregated_lines.append(line)
+
+        with open(aggregated_data_file, "a") as af:
+            af.writelines(aggregated_lines)
+
+        return aggregated_data_file
