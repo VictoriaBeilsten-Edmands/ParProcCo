854f5004d2932d6753ad42dfc0f86d2711bbcd21
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2021-08-31 11:38:09 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ParProcCo',
     version='1.0',
-    description='Parallel Processing Coordinator. Splits dataset processing to run parallel clister jobs and aggregates outputs',
+    description='Parallel Processing Coordinator. Splits dataset processing to run parallel cluster jobs and aggregates outputs',
     packages=['ParProcCo'],
     install_requires=['h5py', 'numpy'],
 )
