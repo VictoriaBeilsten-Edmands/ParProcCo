2e9fccdb0434f9d2c3b093afe815f8ce00ce33e6
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2021-11-23 11:22:03 +0000

    update requirements in setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version=__version__,
     description='Parallel Processing Coordinator. Splits dataset processing to run parallel cluster jobs and aggregates outputs',
     packages=['ParProcCo'],
-    install_requires=['h5py', 'numpy'],
+    install_requires=['drmaa2', 'h5py', 'numpy', 'parameterized', 'PyYAML'],
 )
