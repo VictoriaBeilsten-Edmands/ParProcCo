9e90d8af99a794f5a80c70cbe306a3fa6316dab1
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2021-11-25 15:40:28 +0000

    Add scripts to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,8 @@ setup(
     packages=find_packages(),
     install_requires=['h5py', 'numpy', 'PyYAML', 'uge-drmaa2'],
     extras_require={'testing': ['parameterized'],},
+    scripts=['scripts/b24_cluster_submit','scripts/bl_cluster_submit', 'scripts/i07_cluster_submit',
+             'scripts/i16_cluster_submit', 'scripts/nxdata_aggregate', 'scripts/p99_cluster_submit',
+             'scripts/ppc_cluster_runner', 'scripts/ppc_cluster_submit'],
     url='https://github.com/DiamondLightSource/ParProcCo',
 )
