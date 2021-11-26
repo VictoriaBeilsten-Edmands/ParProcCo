1a8957af4dd5b8bfb29b55fc7fd63d63826e418c
Peter Chang <peter.chang@diamond.ac.uk>
2021-11-26 17:03:21 +0000

    Replace Diamond specific cluster details with configuration
    Also remove local cluster submit scripts

--- ParProcCo/utils.py
+++ ParProcCo/utils.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import os
 from pathlib import Path
-from typing import Optional, Tuple, Union
+from typing import Dict, List, Optional, Union
 
 
 def check_jobscript_is_readable(jobscript: Path) -> Path:
@@ -54,21 +54,64 @@ def slice_to_string(s: Optional[slice]) -> str:
     step = s.step
     return f"{start}:{stop}:{step}"
 
+from yaml import YAMLObject, SafeLoader
+from dataclasses import dataclass
+
+@dataclass
+class PPCCluster(YAMLObject):
+    yaml_tag = "!PPCCluster"
+    yaml_loader = SafeLoader
+
+    default_queue: str # default cluster queue
+    user_queues: Optional[Dict[str, List[str]]] = None # specific queues with allowed users
+    resources: Optional[Dict[str,str]] = None # job resources
+
+@dataclass
+class PPCConfig(YAMLObject):
+    yaml_tag = "!PPCConfig"
+    yaml_loader = SafeLoader
+
+    allowed_programs: Dict[str,str] # program name, python package with wrapper module
+    project_env_var: str # name of environment variable holding project passed to qsub
+    cluster_help_msg: str # Message to display if cluster commands are not available
+    clusters: Dict[str, PPCCluster] # per cluster configuration, key is UGE_CELL
+
 PPC_YAML='par_proc_co.yaml'
-def get_allowed_programs() -> Tuple[dict[str,str], Path]:
+
+def load_cfg() -> PPCConfig:
     '''
-    Get set of allowed program names from par_proc_co.yaml
-    
+    Load configuration from par_proc_co.yaml
+    --- !PPCConfig
     allowed_programs:
-        rs_map: msmapper_utils
-        blah1: whatever_package1
+        blah1: whatever_package1 # program name: python package (module expected to be called blah1_wrapper and contain a Wrapper class) 
         blah2: whatever_package2
+    project_env_var: CLUSTER_PROJECT # name of environment variable holding project passed to qsub
+    cluster_help_msg: Please module load blah # Message to display if cluster commands are not available
+    clusters:
+        cluster_one: !PPCCluster # cluster name 
+            default_queue: basic.q # default cluster queue
+            user_queues: # dictionary of queues and list of users 
+                better.q: middle_user1
+                best.q: power_user1, power_user2
+        cluster_two: !PPCCluster
+            default_queue: only.q
+            resources:
+                cpu_model: arm64
     '''
     cfg = find_cfg_file(PPC_YAML)
     import yaml
     with open(cfg, 'r') as cff:
-        d = yaml.safe_load(cff)
-        return d['allowed_programs'], cfg
+        ppc_config = yaml.safe_load(cff)
+
+    for ccfg in ppc_config.clusters.values():
+        if ccfg.user_queues:
+            users = set() # check for overlaps
+            for us in ccfg.user_queues.values():
+                common = users.intersection(set(us))
+                if common:
+                    raise ValueError('Users %s cannot be assigned to more than one queue', ', '.join(common))
+                users.update(us)
+    return ppc_config
 
 def find_cfg_file(name: str) -> Path:
     '''
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ParProcCo
+
+Requires a YAML configuration file in grandparent directory of package, CONDA_PREFIX/etc or /etc
+
+
+```
+--- !PPCConfig
+allowed_programs:
+    rs_map: msmapper_utils
+    blah1: whatever_package1
+    blah2: whatever_package2
+project_env_var: CLUSTER_PROJECT
+cluster_help_msg: Please module load blah
+clusters:
+    cluster_one: !PPCCluster
+        default_queue: basic.q
+        user_queues:
+            better.q: middle_user1
+            best.q: power_user1, power_user2
+    cluster_two: !PPCCluster
+        default_queue: only.q
+        resources:
+            cpu_model: arm64
+```
